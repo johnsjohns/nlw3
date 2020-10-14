268106c4ae497eb5e30baeb76e82eaca2021568a
johnsjohns <o-anao@hotmail.com>
2020-10-14 15:29:34 -0300

    Criando e configurando mapa

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,1 +1,26 @@
-var mymap = L.map('mapid').setView([51.505, -0,09], 13);
+//create map
+const map = L.map('mapid').setView([-23.4971759,-46.715966], 16);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon= L.icon({
+    iconUrl:"./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
+
+//create popup overlayu
+
+const popup = L.popup({
+    closeButton: false,
+    className: "map-popup",
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage><img src="./public/images/arrow-white.svg</>')
+//create and add marker
+L.marker([-23.4971759,-46.715966]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup(); 
